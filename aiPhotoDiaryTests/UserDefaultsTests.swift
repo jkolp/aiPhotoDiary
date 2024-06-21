56b8a6290d3a7d670eb8de2b2f230172cf23cb84
Jasper <jkolp.dev@gmail.com>
2024-06-20 22:18:21 -0700

    Added test class

---
+++ aiPhotoDiaryTests/UserDefaultsTests.swift
@@ -0,0 +1,24 @@
+//
+//  UserDefaultsTests.swift
+//  aiPhotoDiaryTests
+//
+//  Created by Jasper on 6/20/24.
+//
+
+import XCTest
+@testable import aiPhotoDiary
+
+final class UserDefaultsTests: XCTestCase {
+
+    func testIsFirstLaunched() {
+        // Given (Arrange)
+        
+        
+        // When (Act)
+
+        
+        // Then (Assert
+
+    }
+    
+}
